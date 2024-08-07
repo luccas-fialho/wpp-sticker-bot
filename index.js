cad849531fbd969cccf1d0d7021c6f3c47b8d6cb
Luccas Fialho <luccas.flh@gmail.com>
2024-08-07 09:09:18 -0300

    feat: soma de numeros

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const qrcode = require('qrcode-terminal')
-const { client } = require('./client')
-const { generateSticker } = require('./generateSticker')
-require('dotenv').config()
+//npm install github:pedroslopez/whatsapp-web.js#webpack-exodus
 
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true })
+const qrcode = require("qrcode-terminal");
+const { client } = require("./client");
+const { generateSticker } = require("./generateSticker");
+require("dotenv").config();
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Online 😎')
+client.on("ready", () => {
+  console.log("Online 😎");
 });
 
 /**
- * Aqui vem como default 'message', bora trocar para 'message_create', 
- * dessa forma nós também poderemos dar comandos e não apenas seus 
+ * Aqui vem como default 'message', bora trocar para 'message_create',
+ * dessa forma nós também poderemos dar comandos e não apenas seus
  * contatos.
  */
-client.on('message_create', msg => {
-    //console.log(msg);
-    const command = msg.body.split(' ')[0];
-    // Cola seu número onde tem o 84848484, sem o 9
-    const sender = msg.from.includes(process.env.NUMBER) ? msg.to : msg.from
-    if (command === ".sticker" || command === ".s") generateSticker(msg, sender)
-    if (command === ".gab") msg.reply("TO CHEGANDO COM OS REFRI!!! 🏃‍♂️🏃‍♀️")
+client.on("message_create", (msg) => {
+  //console.log(msg);
+  const command = msg.body.split(" ")[0];
+  // Cola seu número onde tem o 84848484, sem o 9
+  const sender = msg.from.includes(process.env.NUMBER) ? msg.to : msg.from;
+  if (command === ".sticker" || command === ".s") generateSticker(msg, sender);
+  if (command === ".gab") msg.reply("TO CHEGANDO COM OS REFRI!!! 🏃‍♂️🏃‍♀️");
+  if (msg.body.startsWith('.soma')) {
+    try {
+        // Remover o prefixo '.soma' e dividir a string em números separados por novas linhas
+        const numbers = msg.body.slice(6).split('\n').map(num => parseFloat(num.replace(',', '.').trim()));
+        const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+        msg.reply(`A soma é: R$${sum}`);
+    } catch (error) {
+        console.error('Error processing .soma command:', error);
+        msg.reply('Houve um erro ao processar sua solicitação. Por favor, verifique o formato dos números e tente novamente.');
+    }
+}
 });
 
 client.initialize();
